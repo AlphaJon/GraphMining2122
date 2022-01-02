79478c4e5d652e755eecfea4c9db047b81f66e35
Jonathan Ferreira <jonathan.ferreira@student.unamur.be>
2022-01-02 05:55:12 +0100

    ajout mineurs

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 import numpy as np
 import scipy
 import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+#DateTime	Actor	Recipient	Behavior	Category	Duration	Point
+obs_df = pd.read_csv("OBS_data.txt", sep='\t')
+obs_df.dropna(subset=['Actor', 'Recipient'], inplace=True)
